98c5951bc716f6e4b304e19da435f9ad60254d55
Mikaela T <teetrick33846@students.apsb.org>
2026-01-21 15:44:25 +0000

    videogame added

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,20 +27,43 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+    //toggleGrid();
 
 
     // TODO 2 - Create Platforms
+    createPlatform(700, 200, 20, 540, "purple");
+    createPlatform(1350,400, 50, 50, "lightpink");
+    createPlatform(400, 100, 200, 10, "hotpink");
+    createPlatform(200, 650, 100, 10, "hotpink");
+    createPlatform(100, 550, 100, 10, "hotpink");
+    createPlatform(300, 450, 100, 10, "hotpink");
+    createPlatform(100, 400, 100, 10, "hotpink");
+    createPlatform(200, 263, 100, 10, "hotpink");
+    createPlatform(300, 150, 100, 10, "hotpink");
+    createPlatform(700, 200, 100, 10, "hotpink");
+    createPlatform(900,300, 100, 10, "hotpink");
+    createPlatform(1100, 400, 100, 10, "hotpink");
+    createPlatform(900, 650, 100, 10, "hotpink" );
+    createPlatform(800, 550, 100, 10, "hotpink");
+    createPlatform(720,450, 80, 10, "hotpink");
+    createPlatform(800, 350, 20, 10, "hotpink");
 
 
 
 
     // TODO 3 - Create Collectables
+    createCollectable("database", 1355, 358);
+    createCollectable("database",750, 700);
+    createCollectable("database", 500, 60);
 
 
 
     
     // TODO 4 - Create Cannons
+    createCannon("left", 650, 1000);
+    createCannon("right", 400, 2000);
+    createCannon("top", 300, 3000);
+    
 
 
     
